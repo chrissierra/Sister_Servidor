1a65b01547acae436c781a9c2c47d2305d325509
chrissierra <christopher.sierra@usach.cl>
2018-11-28 22:14:43 -0300

    asdf

--- app/Http/Controllers/libroremuneraciones.php
+++ app/Http/Controllers/libroremuneraciones.php
@@ -11,9 +11,20 @@ class libroremuneraciones extends Controller
 
     	$post = $request->json()->all();
 
-    	$tabla = \App\asistencia::where('usuario_cliente', $post['id'])->get();
-
+    	$tabla = \App\asistencia::where('usuario_cliente', $post['id'])
+    						->where('mes', explode('-', $post['dia'])[0]) // No debe decir $mes + 1 ...; solo $mes 
+                            ->where('anio', explode('-', $post['dia'])[2])
+                            ->where('dia', explode('-', $post['dia'])[1])
+                            ->get();
     	return json_decode($tabla);
+
+    	/*
+    	   $tablaAsistencia = \App\asistencia::where('id_trabajador', $id)
+                            ->where('mes', $mes  ) // No debe decir $mes + 1 ...; solo $mes 
+                            ->where('anio', $anio)
+                            ->where('dia', $dia)
+                            ->where('tipo_movimiento', $mov);
+                            */
 
     }
 }
