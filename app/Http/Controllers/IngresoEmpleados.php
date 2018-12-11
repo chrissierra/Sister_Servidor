d664f925f79975cfadb062fc9811e29e2b8362d6
chrissierra <christopher.sierra@usach.cl>
2018-12-10 22:25:15 -0300

    asdf

--- app/Http/Controllers/IngresoEmpleados.php
+++ app/Http/Controllers/IngresoEmpleados.php
@@ -28,7 +28,7 @@ class IngresoEmpleados extends Controller
         public function Actualizacion_registro_Trabajadores(Request $request){
 
         $post = $request->json()->all();
-        /*
+        
         $planilla =  \App\ingreso_empleados::where('id', $post['id']);
 
         foreach ($post as $key => $value) {
@@ -36,7 +36,7 @@ class IngresoEmpleados extends Controller
         }
 
 
-        $planilla->save();*/
+        $planilla->save();
         echo json_encode($post);
 
     }
