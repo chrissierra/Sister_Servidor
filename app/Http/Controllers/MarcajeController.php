4ba7d3ea515ae61acc605a0c063a2e47869f1778
chrissierra <christopher.sierra@usach.cl>
2018-12-15 08:08:59 -0300

    asdf

--- app/Http/Controllers/MarcajeController.php
+++ app/Http/Controllers/MarcajeController.php
@@ -137,7 +137,71 @@ class MarcajeController extends Controller
       
         $post = $request->json()->all(); // Se ingresa como array EJ: $post["algo"]
       
-        json_encode(var_dump($post));
+        $planilla = \App\ingreso_empleados::where('id', $post['id'])->get();   
+
+        $sucursales = \App\sucursales::where('id', $post['Sucursal'])->get();
+
+        $diferenciaMetros = $this->distance($sucursales[0]['latitud'], $sucursales[0]['longitud'], $post['locacion']['coords']['latitude'], $post['locacion']['coords']['longitude'], 'K');       
+       
+        if($post['movimiento'] == 'entrada'){
+         
+            $tabla_asistencia->rut = $planilla[0]['rut'];
+            $tabla_asistencia->id_trabajador  = $planilla[0]['id'];
+            $tabla_asistencia->tipo_movimiento  = 'entrada';
+            $tabla_asistencia->fecha = date('d/m/Y');
+            $tabla_asistencia->hora = date('H:i:s');
+            $tabla_asistencia->usuario_cliente = $planilla[0]['nombre_empresa_usuario_plataforma'];
+            $tabla_asistencia->nombre = $planilla[0]['nombre'];
+            $tabla_asistencia->apellido = $planilla[0]['apellido'];
+             $tabla_asistencia->status_entrada = $this->GetHoraMovimiento($post['id'], 'entrada', (date('H') + ( date('i') /60 )) , $planilla[0]['horario_con_o_sin_turnos']) ;  // Atraso o no
+            $tabla_asistencia->status_salida = '';  // Atraso o no
+            $tabla_asistencia->cuantia_entrada = date('H')+ (date('i') /60 ) ;
+            $tabla_asistencia->cuantia_salida = '';
+            $tabla_asistencia->mes = date('m');
+            $tabla_asistencia->anio = date('Y');
+            $tabla_asistencia->dia = date('d');
+            $tabla_asistencia->cuantia_esperada = $this->cuantia_esperada;
+            $tabla_asistencia->cuantia_diferencia_real_esperada = $this->cuantia_diferencia_real_esperada;
+            $tabla_asistencia->tiempo = time();
+            $tabla_asistencia->locacion = $sucursales[0]['nombre'];
+            $tabla_asistencia->latitude = $post['locacion']['coords']['latitude'];
+            $tabla_asistencia->longitude = $post['locacion']['coords']['longitude'];
+            $tabla_asistencia->altitude = $post['locacion']['coords']['altitude'];
+            $tabla_asistencia->url = $post['url'];
+            $tabla_asistencia->distancia = $diferenciaMetros;
+            $tabla_asistencia->save();
+            echo json_encode('EntradaRealizada');
+        }
+        
+         if($post['movimiento'] == 'salida'){
+            $tabla_asistencia->rut = $planilla[0]['rut'];
+            $tabla_asistencia->id_trabajador  = $planilla[0]['id'];
+            $tabla_asistencia->tipo_movimiento  = 'salida';
+            $tabla_asistencia->fecha = date('d/m/Y');
+            $tabla_asistencia->hora = date('H:i:s');
+            $tabla_asistencia->usuario_cliente = $planilla[0]['nombre_empresa_usuario_plataforma'];
+            $tabla_asistencia->nombre = $planilla[0]['nombre'];
+            $tabla_asistencia->apellido = $planilla[0]['apellido'];
+            $tabla_asistencia->status_entrada = '';  // Atraso o no
+            $tabla_asistencia->status_salida = $this->GetHoraMovimiento($post['id'], 'salida', (date('H') + ( date('i') /60 )) , $planilla[0]['horario_con_o_sin_turnos']) ;  // Atraso o no
+            $tabla_asistencia->cuantia_entrada = '' ;
+            $tabla_asistencia->cuantia_salida = date('H') + ( date('i') /60 );
+            $tabla_asistencia->mes = date('m');
+            $tabla_asistencia->anio = date('Y');
+            $tabla_asistencia->dia = date('d');
+            $tabla_asistencia->cuantia_esperada = $this->cuantia_esperada;
+            $tabla_asistencia->cuantia_diferencia_real_esperada = $this->cuantia_diferencia_real_esperada;
+            $tabla_asistencia->tiempo = time();
+            $tabla_asistencia->locacion = $sucursales[0]['nombre'];
+            $tabla_asistencia->latitude = $post['locacion']['coords']['latitude'];
+            $tabla_asistencia->longitude = $post['locacion']['coords']['longitude'];
+            $tabla_asistencia->altitude = $post['locacion']['coords']['altitude'];
+            $tabla_asistencia->url = $post['url'];
+            $tabla_asistencia->distancia = $diferenciaMetros;
+            $tabla_asistencia->save();
+            echo json_encode('SalidaRealizada');
+
+        }
          // echo json_encode($Salida);
         //echo json_encode($this->fecha);
 
