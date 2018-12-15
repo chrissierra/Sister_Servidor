cb9b3cf5437c01832896178c358f7d9255d46faa
chrissierra <christopher.sierra@usach.cl>
2018-12-14 22:42:41 -0300

    asdf

--- app/Http/Controllers/MarcajeController.php
+++ app/Http/Controllers/MarcajeController.php
@@ -137,7 +137,7 @@ class MarcajeController extends Controller
       
         $post = $request->json()->all(); // Se ingresa como array EJ: $post["algo"]
       
-        json_encode($post);
+        json_encode(var_dump($post));
          // echo json_encode($Salida);
         //echo json_encode($this->fecha);
 
