8e7835469a2740f66744e7f5ad89e58cfceb1308
chrissierra <christopher.sierra@usach.cl>
2018-12-21 17:49:12 -0300

    adf

--- app/Http/Controllers/hitosController.php
+++ app/Http/Controllers/hitosController.php
@@ -9,8 +9,22 @@ class hitosController extends Controller
     //
 
     public function ingresarHitos(Request $request){
+
     	$post = $request->json()->all();
-    	echo json_encode($post);
+    	$hitos =  new \App\hitos;
+    	echo json_encode($post['nombre_empresa']);
+    /*	$hitos->tipo_hito = $post[0]['value'];
+    	$hitos->nombre_empresa = $post[1]['value'];
+    	$hitos->mandante = $post[2]['value'];
+    	$hitos->url1 = $post[3]['value'];
+    	$hitos->url2 = $post[3]['value'];
+    	$hitos->url3 = $post[3]['value'];
+    	$hitos->comentario =$post[4]['value'];
+    	$hitos->nombre_trabajador =$post[4]['value'];
+    	$hitos->trabajador_id =$post[4]['value'];
+
+    	$hitos->save();
+    	echo json_encode(array("estatus"=>'ok'));*/
     }
 
 
