999fb4732cb2959eb610015f2a738e70acf70730
chrissierra <christopher.sierra@usach.cl>
2018-12-22 19:50:19 -0300

    adf

--- app/Http/Controllers/mandanteController.php
+++ app/Http/Controllers/mandanteController.php
@@ -17,7 +17,7 @@ class mandanteController extends Controller
 
     	$mandantes->nombre_mandante = $post[0]['value'];
     	$mandantes->rut_mandante = $post[1]['value'];
-    	$mandantes->clave = $post[2]['value'];
+    	$mandantes->clave = password_hash($post[2]['value'], PASSWORD_DEFAULT);
     	$mandantes->proveedor_servicios = $post[3]['value'];
     	$mandantes->hitos =$post[4]['value'];
     	$mandantes->save();
@@ -34,7 +34,7 @@ class mandanteController extends Controller
 
     	$mandantes->update(['nombre_mandante' => $post[0]['value']]);
     	$mandantes->update(['rut_mandante' => $post[1]['value']]);
-    	$mandantes->update(['clave' => $post[2]['value']]);
+    	$mandantes->update(['clave' => password_hash($post[2]['value'], PASSWORD_DEFAULT)  ]); 
     	$mandantes->update(['proveedor_servicios' => $post[3]['value']]);
     	$mandantes->update(['hitos' =>$post[4]['value']]);
 
@@ -64,5 +64,25 @@ class mandanteController extends Controller
         } 
         return response()->json($mandantes);
     }
+
+            public function getMandantePorRut(Request $request){
+                    $post = $request->json()->all();
+                    $logueos = \App\mandantes::where('proveedor_servicios', $post['proveedor_servicios'])
+                    ->where('clave', $post['clave'])
+                    
+                      if($logueos->count() == 0){
+        
+                            abort(403, 'Unauthorized action.');
+                            
+                            }else{
+
+                                if (password_verify($post["clave"], $logueos->get()[0]['password'] )) {
+                                echo json_encode(array("rut_empresa"=>$logueos->get()[0]["rut_empresa"],"id"=>$logueos->get()[0]["id"], "nombre_empresa"=>$logueos->get()[0]["nombre_empresa"],"nombre_rep"=>$logueos->get()[0]["nombre_rep"]));
+                            } else {
+                                echo json_encode(array("error"=>'Contraseña Errónea'));
+                            }
+                            
+                            }
+           }
 
 }
