38ed00162eaed6f5f3f775eafafcc1e626fc86c7
chrissierra <christopher.sierra@usach.cl>
2018-12-27 12:51:20 -0300

    asdf

--- app/Http/Controllers/desvinculadosController.php
+++ app/Http/Controllers/desvinculadosController.php
@@ -9,11 +9,11 @@ class desvinculadosController extends Controller
     //
     public function desvincular(Request $request){
 
-
+		$post = $request->json()->all();
+    	
     	$planilla =  \App\ingreso_empleados::where('id', $post['id']);
 
     	$tablaDesvinculacion = new \App\TrabajadoresDesvinculados; 
-
 
     	foreach ($planilla->get()->toArray() as $key => $value) {
     		$tablaDesvinculacion->$key = $value;
