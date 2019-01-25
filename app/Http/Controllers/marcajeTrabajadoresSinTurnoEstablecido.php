b778734f236c9271c3748c22fc4b947919489ba3
chrissierra <christopher.sierra@usach.cl>
2019-01-25 11:47:56 -0300

    Por fin, Gracias a Dios, solución de caso de Claudio muñoz. Marcaba su entrada sin que hubieran pasado 12 horas, se redujo a 8 horas

--- app/Http/Controllers/marcajeTrabajadoresSinTurnoEstablecido.php
+++ app/Http/Controllers/marcajeTrabajadoresSinTurnoEstablecido.php
@@ -33,7 +33,7 @@ class marcajeTrabajadoresSinTurnoEstablecido extends Controller
              if($ultimoMovimiento->count() === 0 ){
               	# Entrada
               	echo json_encode('entrada');
-              }elseif($ultimoMovimiento->count() > 0 && $ultimoMovimiento->get()[0]['tipo_movimiento']=== 'salida' && ($this->tiempo- $ultimoMovimiento->get()[0]['tiempo']) <43200){
+              }elseif($ultimoMovimiento->count() > 0 && $ultimoMovimiento->get()[0]['tipo_movimiento']=== 'salida' && ($this->tiempo- $ultimoMovimiento->get()[0]['tiempo']) <32400){
               	echo json_encode('Listo');
               }elseif($ultimoMovimiento->count() > 0 && $ultimoMovimiento->get()[0]['tipo_movimiento'] === 'entrada'){
               	return $this->analizarMovimiento($ultimoMovimiento->get()[0]);
