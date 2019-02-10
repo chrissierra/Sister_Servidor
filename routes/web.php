f3fe4773f989e696c7ecc25bee47a5972b838d0c
chrissierra <christopher.sierra@usach.cl>
2019-02-10 04:50:24 -0300

    planilla tipo agr

--- routes/web.php
+++ routes/web.php
@@ -909,7 +909,7 @@ $marcador=0;
 
 
 
-    $response = array('nombre'=> $nombre ,'respuesta' => $respuestaNormal, 'rut' => $rut );
+    $response = array('nombre'=> $nombre ,'respuesta' => $respuestaNormal, 'rut' => $rut , 'hola'=> 1);
 
    // echo $horasNoTrabajadas;
     echo json_encode($response);
