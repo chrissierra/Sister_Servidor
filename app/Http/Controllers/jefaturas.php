2a9d9a1144e3edcd666cf0ecc0d5f6388ec57b41
chrissierra <christopher.sierra@usach.cl>
2019-02-19 20:28:10 -0300

    fhg

--- app/Http/Controllers/jefaturas.php
+++ app/Http/Controllers/jefaturas.php
@@ -11,7 +11,9 @@ class jefaturas extends Controller
      public function ingresarjefatura(Request $request){
     	$post = $request->json()->all();
     	$jefaturas = new \App\jefaturas;
-    	
+    	$centro_costo = \App\centro_de_costo::where('id', $post[1]['value'])->get();
+        $departamento = \App\departamento::where('id', $post[2]['value'])->get();
+
     	for ($i=0; $i < count($post); $i++) { 
     		if( strlen($post[$i]['value']) < 1) abort(403, 'Unauthorized action.');
     	}
@@ -19,6 +21,9 @@ class jefaturas extends Controller
     	$jefaturas->nombre= $post[0]['value'];
     	$jefaturas->centro_costo_id = $post[1]['value'];
     	$jefaturas->departamento_id = $post[2]['value'];
+        $jefaturas->nombre_empresa = $post[3]['value'];
+        $jefaturas->centro_costo_nombre = $centro_costo[0]['nombre'];
+        $jefaturas->departamento_nombre = $departamento[0]['nombre'];
     	$jefaturas->save();
     	echo json_encode(array("estatus"=>'ok'));
     	//$planilla = new \App\mandantes;
@@ -28,11 +33,12 @@ class jefaturas extends Controller
 
     public function actualizarjefatura(Request $request){
     	$post = $request->json()->all();
-    	$jefaturas = \App\jefaturas::where('jefatura', $post[0]['value'])
-    	 						->where('nombre_empresa', $post[1]['value']);    	
+    	$jefaturas = \App\jefaturas::where('nombre_empresa', $post[0]['id_valor']);   	
 		$jefaturas->update(['nombre' => $post[0]['value']]);
     	$jefaturas->update(['centro_costo_id' => $post[4]['value']]);
     	$jefaturas->update(['departamento_id' => $post[5]['value']]);
+        $jefaturas->update(['centro_costo_nombre' => $centro_costo[0]['nombre']]);
+        $jefaturas->update(['departamento_nombre' => $departamento[0]['nombre']]);
     
 
     }
@@ -46,7 +52,6 @@ class jefaturas extends Controller
 
     public function deletejefaturas(Request $request){
     	$post = $request->json()->all();
-    	$jefaturas = \App\jefaturas::where('jefatura', $post[0]['value'])
-    	 						->where('nombre_empresa', $post[1]['value'])->delete();
+    	$jefaturas = \App\jefaturas::where('id', $post[0]['id_valor'])->delete();
     }
 }
