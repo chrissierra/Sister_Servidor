e0dc1714faaebb457526fb904a674609bc076e75
chrissierra <christopher.sierra@usach.cl>
2019-02-24 11:38:29 -0300

    Avanzando..

--- app/Http/Controllers/departamento.php
+++ app/Http/Controllers/departamento.php
@@ -12,6 +12,7 @@ class departamento extends Controller
     	$post = $request->json()->all();
     	$departamento = new \App\departamento;
     	$centro_costo = \App\centro_de_costo::where('id', $post[1]['value'])->get();
+        $jefatura = \App\jefatura::where('id', $post[2]['value'])->get();
     	for ($i=0; $i < count($post); $i++) { 
     		if( strlen($post[$i]['value']) < 1) abort(403, 'Unauthorized action.');
     	}
@@ -22,6 +23,7 @@ class departamento extends Controller
     	$departamento->jefatura_id = $post[2]['value'];
     	$departamento->nombre_empresa = $post[3]['value'];
     	$departamento->trabajador_encargado_id = $post[4]['value'];
+        $departamento->jefatura_nombre = $jefatura[0]['nombre'];
 	    $departamento->save();
     	echo json_encode(array("estatus"=>'ok'));
     	//$planilla = new \App\mandantes;
@@ -31,14 +33,18 @@ class departamento extends Controller
 
     public function actualizardepartamento(Request $request){
     	$post = $request->json()->all();
-    	$centro_costo = \App\centro_de_costo::where('id', $post[1]['value'])->get();
-    	$departamento = \App\departamento::where('id', $post[0]['id_valor']);  	
+        $departamento = \App\departamento::where('id', $post[0]['id_valor']); 
+
+    	$centro_costo = \App\centro_de_costo::where('id', $post[1]['value'])->get();   	
+        $jefatura = \App\jefatura::where('id', $post[2]['value'])->get();
+ 	
 		$departamento->update(['nombre' => $post[0]['value']]);
     	$departamento->update(['centrocosto_id' => $post[1]['value']]);
     	$departamento->update(['centro_costo_nombre' => $centro_costo[0]['nombre']]);
     	$departamento->update(['jefatura_id' => $post[2]['value']]);
     	$departamento->update(['nombre_empresa' => $post[3]['value']]);
     	$departamento->update(['trabajador_encargado_id' => $post[4]['value']]);
+        $departamento->update(['jefatura_nombre' => $jefatura[0]['nombre']]);
     
 
     }
