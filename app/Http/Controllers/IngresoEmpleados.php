6e19a7e508a42217e339b8845a262327233ef355
chrissierra <christopher.sierra@usach.cl>
2019-04-22 22:40:26 -0400

    ASD

--- app/Http/Controllers/IngresoEmpleados.php
+++ app/Http/Controllers/IngresoEmpleados.php
@@ -10,9 +10,10 @@ class IngresoEmpleados extends Controller
     public function Enrolamiento(Request $request){
 
     	$post = $request->json()->all();
+
         $jefatura = \App\jefaturas::where('id', $post['jefatura_id'])->get();
         $cargos = \App\cargos::where('id', $post['cargo_id'])->get();
-
+        $sucursal_nombre = \App\sucursales::where('id', $post['sucursal_id'])->get();
     	$planilla = new \App\ingreso_empleados;
 
     	foreach ($post as $key => $value) {
@@ -21,6 +22,7 @@ class IngresoEmpleados extends Controller
 
         $planilla->jefatura = $jefatura[0]['nombre'];
         $planilla->cargo_nombre = $cargos[0]['cargo'];
+        $planilla->sucursal_nombre = $sucursal_nombre[0]['nombre'];
 
 
 
@@ -37,12 +39,14 @@ class IngresoEmpleados extends Controller
         $post = $request->json()->all();
 
         $planilla =  \App\ingreso_empleados::where('id', $post['id']);
-
+        
+        $sucursal_nombre = \App\sucursales::where('id', $post['sucursal_id'])->get();
+        
         foreach ($post as $key => $value) {
             $planilla->update([$key => $value]);
         }
 
-
+        $planilla->update(['sucursal_nombre' => $sucursal_nombre[0]['nombre']]);
         echo json_encode($post);
 
     }
