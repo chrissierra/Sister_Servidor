60f4a54faa2ab0f6be68f5acbc28c1ece950b2f7
chrissierra <christopher.sierra@usach.cl>
2019-07-16 15:51:36 -0400

    Adsf

--- app/ingreso_empleados.php
+++ app/ingreso_empleados.php
@@ -9,4 +9,10 @@ class ingreso_empleados extends Model
     //
     protected $table = 'ingreso_empleados'; 
     public $timestamps = false;
+    /**
+* The attributes that aren't mass assignable.
+*
+* @var array
+*/
+protected $guarded = [];
 }
