a4444de44002a02ae366a493e1eef1457c4647c7
chrissierra <christopher.sierra@usach.cl>
2019-07-20 21:52:47 -0400

    ASDF

--- app/Http/Controllers/ContrasteFotograficoValidacion.php
+++ app/Http/Controllers/ContrasteFotograficoValidacion.php
@@ -43,8 +43,18 @@ class ContrasteFotograficoValidacion extends Controller
 
     public function UpdateContrasteFotograficoValidacion(Request $request, contraste_fotografico_validacion $contraste_fotografico_validacion)
     {
+        $post = $request->json()->all(); // Se ingresa como array EJ: $post["algo"]  
 
-            dd($request);
+        //var_dump($post); 
+
+        //echo "Que pasa...";     
+
+        $get_validaciones = $contraste_fotografico_validacion::where('trabajador_id', $post['trabajador_id'])->get();
+        $get_validaciones->updata(['validado'=> $post['validado'] ]);
+        return response()->json(
+                  ['response' => 'Ok' ]
+        );
+    
 
     }
 
