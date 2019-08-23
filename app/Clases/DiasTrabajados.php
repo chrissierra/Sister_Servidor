26b395ed3f67f819f38547801212a18f14ba1ae4
chrissierra <christopher.sierra@usach.cl>
2019-08-23 13:43:29 -0400

    asdf

--- app/Clases/DiasTrabajados.php
+++ app/Clases/DiasTrabajados.php
@@ -70,10 +70,10 @@ class DiasTrabajados {
                 ->where('anio',  $anio)
                 ->where('dia',  $matches)
                 ->where('tipo_movimiento',  strtolower ($array["tipo_a_".$i]))
-                 ->get();
+                 
 
                 if($ultimoMovimiento->count()>0){
-                	echo "Si Trabajó...". $ultimoMovimiento[0]->nombre  . "<br>";
+                	echo "Si Trabajó...". $ultimoMovimiento->get()[0]->nombre  . "<br>";
                 }else{
                 	echo "falto...<br>";
                 }
@@ -94,10 +94,10 @@ class DiasTrabajados {
                 ->where('anio',  $anio)
                 ->where('dia',  $matches)
                 ->where('tipo_movimiento',  strtolower ($array["tipo_a_".$i]))
-                ->get();
+                
 
                 if($ultimoMovimiento->count()>0){
-                	echo "Si Trabajó...". $ultimoMovimiento[0]->nombre . "<br>";
+                	echo "Si Trabajó...". $ultimoMovimiento->get()[0]->nombre . "<br>";
                 }else{
                 	echo "falto...<br>";
                 }
