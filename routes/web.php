deb673f87b3f3d516fbfe658c193f01a448e1ccb
chrissierra <christopher.sierra@usach.cl>
2019-08-30 18:19:15 -0400

    asdf

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::get('/test/{id}/{mes}/{anio}', function ($id, $mes, $anio) {
 
 });
 
-Route::get('/presta', function ($id, $mes, $anio) {
+Route::get('/presta', function () {
     Fpdf::AddPage();
     Fpdf::SetFont('Courier', 'B', 18);
     Fpdf::Cell(50, 25, 'Hello World!');
