370a8bda551180d55ddb59412a5f804d7477954b
chrissierra <christopher.sierra@usach.cl>
2019-09-25 10:55:16 -0300

    asd

--- app/Http/Controllers/ResExtraController.php
+++ app/Http/Controllers/ResExtraController.php
@@ -31,7 +31,7 @@ class ResExtraController extends Controller
         //
 		$post = $request->json()->all(); 
         $response = \App\res_extra::where('id', $post['id']);         
-        return json_encode(array('response'=>  $response->get(), 'ok' => true, 'empresa'=> $response->empresa()->get()  ));
+        return json_encode(array('response'=>  $response->get(), 'ok' => true, 'empresa'=> $response->get()->empresa()->get()  ));
 
     } 
 
